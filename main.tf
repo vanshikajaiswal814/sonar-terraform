b2817086459c4e9b4ae090cbd8d448767df4bb54
vanshikajaiswal814 <63871591+vanshikajaiswal814@users.noreply.github.com>
2024-07-15 15:35:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,225 +1,93 @@
-provider "aws" {
-  region = local.region
-}
-
-data "aws_caller_identity" "current" {}
-data "aws_availability_zones" "available" {}
-
 locals {
-  name    = "complete-postgresql"
-  region  = "eu-west-1"
-  region2 = "eu-central-1"
-
-  vpc_cidr = "10.0.0.0/16"
-  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+  resource_names = {
+    transit_rg_name                   = lower(join("-", [var.cloudspace_id, var.region, var.application_name, var.transit_rg_name]))
+    key_vault_name                    = lower(join("-", [var.cloudspace_id, var.region, var.application_name, var.transit_key_vault_name]))
+    postgresdb_name                   = lower(join("-", [var.postgres_flexible_server_name, var.application_name]))
+    postgresql_dedicated_subnet_name  = lower(join("-", [var.postgres_flexible_server_name, var.application_name, "subnet"]))
+    postgresql_private_dns_name       = lower(join("-", [var.postgres_flexible_server_name, var.application_name, "pdz.postgres.database.azure.com"]))
+    postgresql_private_dns_v_net_link = lower(join("-", [var.postgres_flexible_server_name, var.application_name, "pdzvnetlink.com"]))
+    postgresql_database               = lower(join("-", [var.postgres_flexible_server_name, var.application_name, "database"]))
+  }
 
   tags = {
-    Name       = local.name
-    Example    = local.name
-    Repository = "https://github.com/terraform-aws-modules/terraform-aws-rds"
+    Customer           = var.customer
+    BusinessUnit       = var.business_unit
+    ApplicationName    = var.applicationname
+    DataClassification = var.data_classification
+    Environment        = var.environment
+    OwnerName          = var.owner_name
+    Contact            = var.contact
   }
 }
 
-################################################################################
-# RDS Module
-################################################################################
+# module "resource_group" {
+#   source   = "./modules/resource_group"
+#   name     = local.resource_names.transit_rg_name
+#   location = var.location
+#   tags     = merge({ "ResourceName" = format("%s", local.resource_names.transit_rg_name) }, local.tags)
+# }
 
-module "db" {
-  source = "../../"
+# module "argocd" {
+#   source                 = "./modules/argocd"
+#   app_client_oidc_id     = var.app_client_oidc_id
+#   app_client_oidc_secret = var.app_client_oidc_secret
+# }
 
-  identifier = local.name
-
-  # All available versions: https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_PostgreSQL.html#PostgreSQL.Concepts
-  engine               = "postgres"
-  engine_version       = "14"
-  family               = "postgres14" # DB parameter group
-  major_engine_version = "14"         # DB option group
-  instance_class       = "db.t4g.large"
-
-  allocated_storage     = 20
-  max_allocated_storage = 100
-
-  # NOTE: Do NOT use 'user' as the value for 'username' as it throws:
-  # "Error creating DB Instance: InvalidParameterValue: MasterUsername
-  # user cannot be used as it is a reserved word used by the engine"
-  db_name  = "completePostgresql"
-  username = "complete_postgresql"
-  port     = 5432
-
-  # Setting manage_master_user_password_rotation to false after it
-  # has previously been set to true disables automatic rotation
-  # however using an initial value of false (default) does not disable
-  # automatic rotation and rotation will be handled by RDS.
-  # manage_master_user_password_rotation allows users to configure
-  # a non-default schedule and is not meant to disable rotation
-  # when initially creating / enabling the password management feature
-  manage_master_user_password_rotation              = true
-  master_user_password_rotate_immediately           = false
-  master_user_password_rotation_schedule_expression = "rate(15 days)"
-
-  multi_az               = true
-  db_subnet_group_name   = module.vpc.database_subnet_group
-  vpc_security_group_ids = [module.security_group.security_group_id]
-
-  maintenance_window              = "Mon:00:00-Mon:03:00"
-  backup_window                   = "03:00-06:00"
-  enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
-  create_cloudwatch_log_group     = true
-
-  backup_retention_period = 1
-  skip_final_snapshot     = true
-  deletion_protection     = false
-
-  performance_insights_enabled          = true
-  performance_insights_retention_period = 7
-  create_monitoring_role                = true
-  monitoring_interval                   = 60
-  monitoring_role_name                  = "example-monitoring-role-name"
-  monitoring_role_use_name_prefix       = true
-  monitoring_role_description           = "Description for monitoring role"
-
-  parameters = [
-    {
-      name  = "autovacuum"
-      value = 1
-    },
-    {
-      name  = "client_encoding"
-      value = "utf8"
-    }
-  ]
-
-  tags = local.tags
-  db_option_group_tags = {
-    "Sensitive" = "low"
-  }
-  db_parameter_group_tags = {
-    "Sensitive" = "low"
-  }
+module "key_vault" {
+  source                        = "./modules/key_vault"
+  key_vault_name                = local.resource_names.key_vault_name
+  key_vault_default_subnet_name = var.key_vault_default_subnet_name
+  location                      = var.location
+  managed_resource_group        = var.managed_resource_group
+  existing_v_net_name           = var.existing_v_net_name
+  deployment_resource_group     = var.deployment_resource_group
+  tags                          = merge({ "ResourceName" = format("%s", local.resource_names.key_vault_name) }, local.tags)
 }
 
-module "db_default" {
-  source = "../../"
-
-  identifier                     = "${local.name}-default"
-  instance_use_identifier_prefix = true
-
-  create_db_option_group    = false
-  create_db_parameter_group = false
-
-  # All available versions: https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_PostgreSQL.html#PostgreSQL.Concepts
-  engine               = "postgres"
-  engine_version       = "14"
-  family               = "postgres14" # DB parameter group
-  major_engine_version = "14"         # DB option group
-  instance_class       = "db.t4g.large"
-
-  allocated_storage = 20
-
-  # NOTE: Do NOT use 'user' as the value for 'username' as it throws:
-  # "Error creating DB Instance: InvalidParameterValue: MasterUsername
-  # user cannot be used as it is a reserved word used by the engine"
-  db_name  = "completePostgresql"
-  username = "complete_postgresql"
-  port     = 5432
-
-  db_subnet_group_name   = module.vpc.database_subnet_group
-  vpc_security_group_ids = [module.security_group.security_group_id]
-
-  maintenance_window      = "Mon:00:00-Mon:03:00"
-  backup_window           = "03:00-06:00"
-  backup_retention_period = 0
-
-  tags = local.tags
+module "postgresql-server" {
+  source                           = "./modules/PostgreSQL_module"
+  deployment_resource_group        = var.deployment_resource_group
+  managed_resource_group           = var.managed_resource_group
+  postgres_sql_server_name         = local.resource_names.postgresdb_name
+  key_vault                        = local.resource_names.key_vault_name
+  existing_v_net_name              = var.existing_v_net_name
+  postgresql_dedicated_subnet_name = local.resource_names.postgresql_dedicated_subnet_name
+  private_dns_name                 = local.resource_names.postgresql_private_dns_name
+  private_dns_v_net_link           = local.resource_names.postgresql_private_dns_v_net_link
+  postgres_db_username             = var.postgres_db_username
+  database                         = local.resource_names.postgresql_database
+  postgres_password_key            = var.postgres_password_key
+  postgres_username_key            = var.postgres_username_key
+  address_prefixes                 = var.address_prefixes
+  tags                             = merge({ "ResourceName" = format("%s", local.resource_names.postgresdb_name) }, local.tags)
+  depends_on                       = [module.key_vault]
 }
 
-module "db_disabled" {
-  source = "../../"
-
-  identifier = "${local.name}-disabled"
-
-  create_db_instance        = false
-  create_db_parameter_group = false
-  create_db_option_group    = false
+module "acr_module" {
+  source                    = "./modules/acr_module"
+  acr_name                  = var.acr_name
+  deployment_resource_group = var.deployment_resource_group
+  managed_resource_group    = var.managed_resource_group
+  location                  = var.location
+  existing_v_net_name       = var.existing_v_net_name
+  acr_sku                   = var.acr_sku
+  acr_public_network_access = var.acr_public_network_access
+  acr_admin_enabled         = var.acr_admin_enabled
+  default_subnet_name       = var.default_subnet_name
+  acr_private_dns_name      = var.acr_private_dns_name
+  tags                      = merge({ "ResourceName" = format("%s", var.acr_name) }, local.tags)
 }
 
-################################################################################
-# RDS Automated Backups Replication Module
-################################################################################
-
-provider "aws" {
-  alias  = "region2"
-  region = local.region2
-}
-
-module "kms" {
-  source      = "terraform-aws-modules/kms/aws"
-  version     = "~> 1.0"
-  description = "KMS key for cross region automated backups replication"
-
-  # Aliases
-  aliases                 = [local.name]
-  aliases_use_name_prefix = true
-
-  key_owners = [data.aws_caller_identity.current.arn]
-
-  tags = local.tags
-
-  providers = {
-    aws = aws.region2
-  }
-}
-
-module "db_automated_backups_replication" {
-  source = "../../modules/db_instance_automated_backups_replication"
-
-  source_db_instance_arn = module.db.db_instance_arn
-  kms_key_arn            = module.kms.key_arn
-
-  providers = {
-    aws = aws.region2
-  }
-}
-
-################################################################################
-# Supporting Resources
-################################################################################
-
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 5.0"
-
-  name = local.name
-  cidr = local.vpc_cidr
-
-  azs              = local.azs
-  public_subnets   = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k)]
-  private_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 3)]
-  database_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 6)]
-
-  create_database_subnet_group = true
-
-  tags = local.tags
-}
-
-module "security_group" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "~> 5.0"
-
-  name        = local.name
-  description = "Complete PostgreSQL example security group"
-  vpc_id      = module.vpc.vpc_id
-
-  # ingress
-  ingress_with_cidr_blocks = [
-    {
-      from_port   = 5432
-      to_port     = 5432
-      protocol    = "tcp"
-      description = "PostgreSQL access from within VPC"
-      cidr_blocks = module.vpc.vpc_cidr_block
-    },
-  ]
-
-  tags = local.tags
+module "Storage-account" {
+  source                           = "./modules/Storage-account"
+  storage_account_name             = var.storage_account_name
+  existing_v_net_name              = var.existing_v_net_name
+  customer_managed_key             = var.customer_managed_key
+  deployment_resource_group        = var.deployment_resource_group
+  share_name                       = var.share_name
+  managed_resource_group           = var.managed_resource_group
+  location                         = var.location
+  key_vault                        = local.resource_names.key_vault_name
+  storage_account_private_dns_name = var.storage_account_private_dns_name
+  depends_on                       = [module.key_vault]
 }
